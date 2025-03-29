4597ddb1c1091e528c4078796f0029b1f1e2ff04
Deepak Vishwakarma <77259216+Deepakvishwakarma1@users.noreply.github.com>
2025-03-29 19:17:58 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,25 +1,40 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
+**Seeding for reproducibility**
+
+  # Set seeds for reproducibility
+import random
+random.seed(0)
+
 import numpy as np
+np.random.seed(0)
+
+import tensorflow as tf
+tf.random.set_seed(0)
+
+
+**Importing the dependencies**
 import os
+import json
+from zipfile import ZipFile
+from PIL import Image
 
-!git clone https://github.com/spMohanty/PlantVillage-Dataset
-!cd PlantVillage-Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
 
-# a code to know the exact no. of images
+**Data Curation**
 
-import os
-# Assuming the PlantVillage-Dataset directory is in the current working directory
-!git clone https://github.com/spMohanty/PlantVillage-Dataset
-dataset_path = 'PlantVillage-Dataset'  # Update with the correct path if different
+*Upload* the kaggle.json file
+!pip install kaggle
 
-if os.path.exists(dataset_path):
-  image_count = 0
-  for root, _, files in os.walk(dataset_path):
-    for file in files:
-      if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
-        image_count += 1
-  print(f"Total number of images found in {dataset_path}: {image_count}")
-else:
-  print(f"Error: Directory '{dataset_path}' not found.")
+kaggle_credentails = json.load(open("kaggle.json"))
+
+# setup Kaggle API key as environment variables
+os.environ['KAGGLE_USERNAME'] = kaggle_credentails["username"]
+os.environ['KAGGLE_KEY'] = kaggle_credentails["key"]
+
+!kaggle datasets download -d abdallahalidev/plantvillage-dataset
+
+ls
+
